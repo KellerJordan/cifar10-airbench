79ba3eac873da3e8459dcf87ed98ac265a554647
KellerJordan <kellerjordan1998@gmail.com>
2024-03-21 00:07:19 +0000

    hardcode whitening patch size as this impacts the final maxpool size which is also hardcoded. move the block widths to hyperparameters

--- airbench94_compiled.py
+++ airbench94_compiled.py
@@ -103,11 +103,12 @@ hyp = {
         'translate': 2,
     },
     'net': {
-        'whitening': {
-            'kernel_size': 2,
+        'widths': {
+            'block1': 64,
+            'block2': 256,
+            'block3': 256,
         },
         'batchnorm_momentum': 0.6,
-        'base_width': 64,
         'scaling_factor': 1/9,
         'tta_level': 2,         # the level of test-time augmentation: 0=none, 1=mirror, 2=mirror+translate
     },
@@ -220,7 +221,7 @@ class Mul(nn.Module):
         return x * self.scale
 
 class BatchNorm(nn.BatchNorm2d):
-    def __init__(self, num_features, eps=1e-12, momentum=hyp['net']['batchnorm_momentum'],
+    def __init__(self, num_features, momentum, eps=1e-12,
                  weight=False, bias=True):
         super().__init__(num_features, eps=eps, momentum=1-momentum)
         self.weight.requires_grad = weight
@@ -240,13 +241,13 @@ class Conv(nn.Conv2d):
         torch.nn.init.dirac_(w[:w.size(1)])
 
 class ConvGroup(nn.Module):
-    def __init__(self, channels_in, channels_out):
+    def __init__(self, channels_in, channels_out, batchnorm_momentum):
         super().__init__()
         self.conv1 = Conv(channels_in,  channels_out)
         self.pool = nn.MaxPool2d(2)
-        self.norm1 = BatchNorm(channels_out)
+        self.norm1 = BatchNorm(channels_out, batchnorm_momentum)
         self.conv2 = Conv(channels_out, channels_out)
-        self.norm2 = BatchNorm(channels_out)
+        self.norm2 = BatchNorm(channels_out, batchnorm_momentum)
         self.activ = nn.GELU()
 
     def forward(self, x):
@@ -263,19 +264,15 @@ class ConvGroup(nn.Module):
 #            Network Definition             #
 #############################################
 
-def make_net():
-    widths = {
-        'block1': (1 * hyp['net']['base_width']), # 64  w/ width at base value
-        'block2': (4 * hyp['net']['base_width']), # 256 w/ width at base value
-        'block3': (4 * hyp['net']['base_width']), # 256 w/ width at base value
-    }
-    whiten_conv_width = 2 * 3 * hyp['net']['whitening']['kernel_size']**2
+def make_net(widths=hyp['net']['widths'], batchnorm_momentum=hyp['net']['batchnorm_momentum']):
+    whiten_kernel_size = 2
+    whiten_width = 2 * 3 * whiten_kernel_size**2
     net = nn.Sequential(
-        Conv(3, whiten_conv_width, kernel_size=hyp['net']['whitening']['kernel_size'], padding=0, bias=True),
+        Conv(3, whiten_width, 2, padding=0, bias=True),
         nn.GELU(),
-        ConvGroup(whiten_conv_width, widths['block1']),
-        ConvGroup(widths['block1'],  widths['block2']),
-        ConvGroup(widths['block2'],  widths['block3']),
+        ConvGroup(whiten_width,     widths['block1'], batchnorm_momentum),
+        ConvGroup(widths['block1'], widths['block2'], batchnorm_momentum),
+        ConvGroup(widths['block2'], widths['block3'], batchnorm_momentum),
         nn.MaxPool2d(3),
         Flatten(),
         nn.Linear(widths['block3'], 10, bias=False),
