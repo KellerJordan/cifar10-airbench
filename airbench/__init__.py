f3c2537bf9794f57e835337b8df292a374cfd981
KellerJordan <kellerjordan1998@gmail.com>
2024-03-22 00:05:32 +0000

    add explicit warmup fns

--- airbench/__init__.py
+++ airbench/__init__.py
@@ -4,3 +4,12 @@ from .lib_airbench95 import train95, make_net95
 from .lib_airbench96 import train96, make_net96
 from .utils import infer, evaluate, CifarLoader
 
+def warmup93(*args, **kwargs):
+    return train93(*args, run=-1, **kwargs)
+def warmup94(*args, **kwargs):
+    return train94(*args, run=-1, **kwargs)
+def warmup95(*args, **kwargs):
+    return train95(*args, run=-1, **kwargs)
+def warmup96(*args, **kwargs):
+    return train96(*args, run=-1, **kwargs)
+
