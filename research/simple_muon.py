f9ae035807774b9fcf2e89cfae9b4ba68cf4c6b8
KellerJordan <kellerjordan1998@gmail.com>
2024-11-26 05:38:06 +0000

    .

--- research/simple_muon.py
+++ research/simple_muon.py
@@ -20,6 +20,8 @@ bs=1000 SGD(wd=0) Muon(lr=0.16, momentum=0.8) -> 93.67 (n=25)
 bs=1000 SGD(wd=0) -> 93.85 (n=20)
 
 wd=0 for head -> 94.00 (n=200)
+Adam(lr=0.01, betas=(0.85, 0.95), wd=0) for head -> 94.01 (n=50)
+^ lr=0.005 -> 93.96 (n=50)
 """
 
 #############################################
@@ -220,7 +222,7 @@ def main(run, model):
     optimizer1 = Muon(filter_params, lr=0.24, momentum=0.6)
     optimizer2 = torch.optim.SGD(norm_biases, lr=lr_biases, weight_decay=wd/lr_biases, momentum=0.85, nesterov=True)
     optimizer3 = torch.optim.SGD([whiten_bias], lr=lr, weight_decay=wd/lr, momentum=0.85, nesterov=True)
-    optimizer4 = torch.optim.SGD([fc_layer], lr=lr, momentum=0.85, nesterov=True)
+    optimizer4 = torch.optim.Adam([fc_layer], lr=0.01, betas=(0.85, 0.95), fused=True)
     def get_lr(step):
         total_train_steps = epochs * len(train_loader)
         return 1 - step / total_train_steps
@@ -244,7 +246,7 @@ def main(run, model):
 
 if __name__ == "__main__":
     model = torch.compile(make_net(), mode='max-autotune')
-    accs = torch.tensor([main(run, model) for run in range(200)])
+    accs = torch.tensor([main(run, model) for run in range(50)])
     print('Mean: %.4f    Std: %.4f' % (accs.mean(), accs.std()))
 
     log_dir = os.path.join('logs', str(uuid.uuid4()))
