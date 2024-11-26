68c7b8b1d86fb808adbcd87899c35ee0c2553c6b
KellerJordan <kellerjordan1998@gmail.com>
2024-11-26 08:24:54 +0000

    .

--- research/simple_muon.py
+++ research/simple_muon.py
@@ -10,14 +10,14 @@ Attains 94.01 mean accuracy (n=200 trials)
 ---------
 Ablations:
 
-Current version -> 94.007 (n=600)
+Old current version -> 94.007 (n=600)
 
 ^ wd=0 for norm_biases -> 93.971 (n=150)
 ^ optimize norm_biases with optimizer3 -> 93.967 (n=300)
 
 ^ init head to zero -> 93.895 (n=300)
 
-^ beta1=0.75 for optim2 and optim3 -> 94.015 (n=300)
+^ beta1=0.75 for optim2 and optim3 -> 94.015 (n=300) [new current version]
 """
 
 #############################################
