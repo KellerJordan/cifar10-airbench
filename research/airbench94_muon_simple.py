00ad7c06a81c5f631b443607ad0b1239d74b87c0
KellerJordan <kellerjordan1998@gmail.com>
2025-03-02 00:53:38 +0000

    .

--- research/airbench94_muon_simple.py
+++ research/airbench94_muon_simple.py
@@ -201,6 +201,6 @@ def main():
     return tta_val_acc
 
 if __name__ == "__main__":
-    accs = torch.tensor([main(run) for run in range(25)])
+    accs = torch.tensor([main() for run in range(25)])
     print('Mean: %.4f    Std: %.4f' % (accs.mean(), accs.std()))
 
